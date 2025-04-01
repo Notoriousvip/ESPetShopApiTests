aef9dc054fd9a32a784776c2cd14ca669e388568
eva <primerxo@mail.ru>
2025-04-02 03:35:39 +0600

    добавила параметризованный тест: Получение списка питомцев по статусу с 5 статусами

--- tests/test_pet.py
+++ tests/test_pet.py
@@ -1,6 +1,7 @@
 import allure
 import jsonschema
 import requests
+import pytest
 from .schemas.pet_schema import PET_SCHEMA
 
 BASE_URL = "http://5.181.109.28:9090/api/v3"
@@ -143,5 +144,26 @@ class TestPet:
 
         with allure.step("Проверка статуса ответа и данных питомца"):
             assert response.status_code == 404
+
+    @allure.title("Получение списка питомцев по статусу")
+    @pytest.mark.parametrize(
+        "status, expected_status_code",
+        [
+            ("available", 200),
+            ("pending", 200),
+            ("sold", 200),
+            ("абракадабра", 400),
+            ("", 400),
+        ]
+    )
+    def test_get_pets_by_status(self, status, expected_status_code):
+        with allure.step(f"Отправка запроса на получение питомцев по статусу {status}"):
+            response = requests.get(url=f"{BASE_URL}/pet/findByStatus", params={"status": status})
+
+        with allure.step("Проверка статуса ответа и формата данных"):
+            assert response.status_code == expected_status_code, (f"Ожидался ответ: {expected_status_code}, а получен {response.status_code}")
+            if response.status_code == 200:
+                assert isinstance(response.json(), list), "Ошибка: ответ должен быть списком"
+
 
 
